4110d02034aaecf0a85d5b6a45778e7571d68234
Eugenio <eugenio.ftt@gmail.com>
2026-03-30 14:16:28 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,4 @@
 #include "pong/PongGame.h"
-// FULL OBJECT ORIENTED
-// Classe Game -> da cui ereditiamo per fare le nostre classi game
-//      class Pong : public Game{};
-        // draw
-        // input
-        // update
-        // tutti gli oggetti di gioco
-
-// class GameObject
-        // posizione
-        // velocity
-        // radius (collisioni)
-            // update
-            // draw
-
-// estendere il vector2
-
-//-Include cartella
-//  -utility
-//  -core
 
 
 int main(){
